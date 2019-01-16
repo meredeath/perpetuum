909f1c978fbc31d055b5a27863a663ec50e4afd2
Meredith <mhu2@stuy.edu>
2019-01-16 00:55:33 -0500

    mostly works, but how do I remove my WKP when I terminate the server?

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
 #include "pipe_networking.h"
 
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    //remove(stoc); how to remove this?? will figure out later
+    exit(1);
+  }
+}
 
 int main() {
 
   int to_client;
   int from_client;
 
-  from_client = server_handshake( &to_client );
+  signal(SIGINT, sighandler);
+  
+  while(1){
+    from_client = server_handshake( &to_client );
+    char *input = calloc(100, 1);
+
+    while(read(from_client, input, 100)){
+      printf("Input from client: %s\n", input);
+
+      //changing client output and sending to server
+      for(int i=0; i<strlen(input); i++){
+	input[i] = input[i] + 'A' - 'a';
+      }
+      write(to_client, input, 100);
+    }
+  }
+  
 }
--- client.c
+++ client.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "pipe_networking.h"
 
 
@@ -7,4 +10,16 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
+  char *message = calloc(200,1);
+  char *response = calloc(200,1);
+  
+  while(1){
+    printf("Enter something you wish to send to the server. \n");
+    fflush(stdout);
+    fgets(message, 100, stdin);
+    message[strlen(message)-1] = "\0";
+    write(to_server, message, 200);
+    read(from_server, response, 200);
+    printf("Server's reply: %s\n", response);
+  }
 }
